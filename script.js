b481eec5f0b8408fb2bd6443c1b2ad3ada4f38b3
SinaSh10 <sinash10.job@gmail.com>
2023-03-13 19:25:25 +0330

    game's rule created

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 let ranNumber=Math.round(Math.random()*10)
-let guessNumber=document.querySelector("#input").value
+document.querySelector("#submit").addEventListener('click',()=>{
+    let guessNumber=Number(document.querySelector("#input").value)
 console.log(ranNumber,guessNumber)
+if(ranNumber==guessNumber){
+    console.log("you are winner")
+}
+else if(ranNumber>guessNumber){
+    console.log("small")
+}
+else if(ranNumber<guessNumber){
+    console.log("big")
+}
+})
