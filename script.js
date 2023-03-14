8f89fdcb13c4dbce5588f40c6ec0d42a1af17659
SinaSh10 <sinash10.job@gmail.com>
2023-03-14 13:29:01 +0330

    wrong and right input

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let ranNumber=Math.round(Math.random()*10)
 document.querySelector("#submit").addEventListener('click',()=>{
     let guessNumber=Number(document.querySelector("#input").value)
+    let status=document.querySelector("#status")
 console.log(ranNumber,guessNumber)
 if(ranNumber==guessNumber){
-    console.log("you are winner")
+    status.innerHTML="you won the game"
+    document.querySelector("#input").disabled="true"
+    document.querySelector('#game').style.backgroundColor="green"
+    document.querySelector('#input').classList.add("win")
+    
 }
 else if(ranNumber>guessNumber){
-    console.log("small")
+    status.innerHTML="your number is small"
+    document.querySelector('#game').style.backgroundColor="red"
+   
+
 }
 else if(ranNumber<guessNumber){
-    console.log("big")
+    status.innerHTML="your number is big"
+    document.querySelector('#game').style.backgroundColor="red"
+    
+    
+
+
 }
 })
